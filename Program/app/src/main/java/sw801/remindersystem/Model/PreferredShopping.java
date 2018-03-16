68f785df9a55530e2ce37f28ea006f0332f70bca
Kaper Helsted <khelst13@student.aau.dk>
2018-03-16 10:13:33 +0100

    fixed minor model error

--- Program/app/src/main/java/sw801/remindersystem/Model/PreferredShopping.java
+++ Program/app/src/main/java/sw801/remindersystem/Model/PreferredShopping.java
@@ -1,29 +1,31 @@
 package sw801.remindersystem.Model;
-import java.util.*;
-public class PreferredShopping{
-    private ArrayList<int> PrefShopList;
+
+import java.util.ArrayList;
+
+public class PreferredShopping {
+    protected ArrayList<Integer> PrefShopList = null;
+
+    public void AddPrefShop(int shopCode) {
+        PrefShopList.add(shopCode);
+    }
+
+    public void removeShopCode(int shopCode) {
+        PrefShopList.remove(shopCode);
+    }
+
     public enum Shops {
         Foetex(3),
-        Bilka (2),
-        Fakta (1),
-        Spar (0)
-        ;
+        Bilka(2),
+        Fakta(1),
+        Spar(0);
         private final int ShopCode;
 
-        Level(int ShopCode) {
+        Shops(int ShopCode) {
             this.ShopCode = ShopCode;
         }
 
         public int getShopCode() {
             return this.ShopCode;
-        }
-
-        public void AddPrefShop(int shopCode) {
-            PrefShopList.add(shopCode)
-        }
-
-        public void removeShopCode(int shopCode) {
-            PrefShopList.remove(shopCode);
         }
     }
 }
