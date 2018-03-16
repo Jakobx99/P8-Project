88b306964474925387073369a880b544c6986efa
SDCAAU <bstaffe@gmail.com>
2018-03-16 09:43:02 +0100

    Added Pref Shopping

--- Program/app/src/main/java/sw801/remindersystem/Model/PreferredShopping.java
+++ Program/app/src/main/java/sw801/remindersystem/Model/PreferredShopping.java
@@ -1,4 +1,30 @@
 package sw801.remindersystem.Model;
-//Enum Existing shopping
+import java.util.*;
+public class PreferredShopping{
+    private ArrayList<int> PrefShopList;
+    public enum Shops {
+        Foetex(3),
+        Bilka (2),
+        Fakta (1),
+        Spar (0)
+        ;
+        private final int ShopCode;
 
+        Level(int ShopCode) {
+            this.ShopCode = ShopCode;
+        }
+
+        public int getShopCode() {
+            return this.ShopCode;
+        }
+
+        public void AddPrefShop(int shopCode) {
+            PrefShopList.add(shopCode)
+        }
+
+        public void removeShopCode(int shopCode) {
+            PrefShopList.remove(shopCode);
+        }
+    }
+}
 
