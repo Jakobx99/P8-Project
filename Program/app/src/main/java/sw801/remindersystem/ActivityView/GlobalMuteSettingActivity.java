371aa5374860f778eb261777d9ce950908bb2d09
Jakob Holgersen <Jakobholgersen@gmail.com>
2018-03-19 14:32:03 +0100

    Changed to fragment

---
+++ Program/app/src/main/java/sw801/remindersystem/ActivityView/GlobalMuteSettingActivity.java
@@ -0,0 +1,15 @@
+package sw801.remindersystem.ActivityView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sw801.remindersystem.R;
+
+public class GlobalMuteSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_mute);
+    }
+}
---
+++ Program/app/src/main/java/sw801/remindersystem/ActivityView/ShoppingSettingActivity.java
@@ -0,0 +1,15 @@
+package sw801.remindersystem.ActivityView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sw801.remindersystem.R;
+
+public class ShoppingSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_setting);
+    }
+}
