f8fc1fd184d0511729254ef1039113af08e19ecb
clubdain <clubdain@hotmail.com>
2018-04-05 11:27:50 +0200

    UI for EditGlobalMute

--- Program/app/src/main/java/sw801/remindersystem/ActivityView/Activity/EditGlobalMuteSettingActivity.java
+++ Program/app/src/main/java/sw801/remindersystem/ActivityView/Activity/EditGlobalMuteSettingActivity.java
@@ -1,17 +1,30 @@
 package sw801.remindersystem.ActivityView.Activity;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import sw801.remindersystem.R;
 
@@ -22,6 +35,11 @@ import sw801.remindersystem.R;
 public class EditGlobalMuteSettingActivity extends AppCompatActivity {
 
     private String globalSettingName;
+    private TextView globalMuteName;
+    static private EditText betweenTime = null;
+    static private EditText betweenTimeTwo = null;
+    static private EditText comment = null;
+    private Button confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,37 +54,117 @@ public class EditGlobalMuteSettingActivity extends AppCompatActivity {
         final EditText editTextName = findViewById(R.id.textInputGlobalMuteName);
         editTextName.setText(globalSettingName);
 
-        editTextName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        final Spinner spinnerLocation = (Spinner) findViewById(R.id.spinnerLocation);
+
+        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    //Remove focus and hide keyboard
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // On selecting a spinner item
+                String item = parent.getItemAtPosition(position).toString();
 
-                    textView.setText(editTextName.getText());
-                    findViewById(R.id.editGlobalMuteLayout).requestFocus();
-
-                    //Hide keyboard
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-
-                    handled = true;
-                }
-                return handled;
+                // Showing selected spinner item
+                Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
             }
         });
 
-        Button confirmButton = findViewById(R.id.button_editGlobalMuteConfirm);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
+        List<String> categoriesLocation = new ArrayList<String>();
+        categoriesLocation.add("No location chosen");
+        categoriesLocation.add("Home");
+        categoriesLocation.add("Work");
+
+
+        ArrayAdapter<String> dataAdapterLocation = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoriesLocation);
+        dataAdapterLocation.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLocation.setAdapter(dataAdapterLocation);
+
+        globalMuteName = findViewById(R.id.textInputGlobalMuteName);
+        betweenTime = findViewById(R.id.editTextTimeBetween);
+        betweenTimeTwo = findViewById(R.id.editTextTimeBetween2);
+        comment = findViewById(R.id.editTextComment);
+        confirm = findViewById(R.id.buttonEditGlobalMuteConfirm);
+
+        // Click events
+        betweenTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String name = editTextName.getText().toString();
-
-                //TODO change in database
-
-                finish();
+            public void onClick(View v) {
+                showTimePickerDialog(v, 1);
+            }
+        });
+        betweenTimeTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v, 2);
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String gName = globalMuteName.getText().toString();
+                String commentText = comment.getText().toString();
+                Integer location = spinnerLocation.getSelectedItemPosition();
+                Long startTime = Long.parseLong(betweenTime.getText().toString());
+                Long endTime = Long.parseLong(betweenTimeTwo.getText().toString());
+                //TODO Connect with ViewModel
+                //List<TIGGER_TYPE>
             }
         });
 
+    }
+    public void showTimePickerDialog(View v, int i) {
+        if (i == 1){
+            DialogFragment newFragment = new EditGlobalMuteSettingActivity.TimePickerFragment1();
+            newFragment.show(getSupportFragmentManager(), "timePicker");
+        }
+        else{
+            DialogFragment newFragment = new EditGlobalMuteSettingActivity.TimePickerFragment2();
+            newFragment.show(getSupportFragmentManager(), "timePicker");
+        }
+
+    }
+
+    public static class TimePickerFragment1 extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String time = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+            betweenTime.setText(time);
+        }
+    }
+
+    public static class TimePickerFragment2 extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String time = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+            betweenTimeTwo.setText(time);
+        }
     }
 }
