84a97667be55760726eda01f290c3e4ceb98bf78
SDCAAU <bstaffe@gmail.com>
2018-04-10 10:41:40 +0200

    EditSmartDevice

--- Program/app/src/main/java/p8project/sw801/ui/SmartDevice/EditSmartDeviceActivity.java
+++ Program/app/src/main/java/p8project/sw801/ui/SmartDevice/EditSmartDeviceActivity.java
@@ -1,10 +1,12 @@
 package p8project.sw801.ui.SmartDevice;
 
 import android.app.Activity;
+import android.arch.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -12,19 +14,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-
+import javax.inject.Inject;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+import p8project.sw801.BR;
 import p8project.sw801.R;
+import p8project.sw801.databinding.ActivityAddSmartDeviceBinding;
+import p8project.sw801.ui.base.BaseActivity;
 
-public class EditSmartDeviceActivity extends AppCompatActivity {
+public class EditSmartDeviceActivity extends BaseActivity<ActivityAddSmartDeviceBinding, AddSmartDeviceViewModel> implements AddSmartDeviceNavigator, HasSupportFragmentInjector {
     private Integer deviceId;
+    @Inject
+    DispatchingAndroidInjector<Fragment> fragmentDispatchingAndroidInjector;
+    @Inject
+    ViewModelProvider.Factory mViewModelFactory;
+    private ActivityAddSmartDeviceBinding mActivityEditSmartDeviceBinding;
+    private AddSmartDeviceViewModel mMainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_smart_device);
-        Intent i = getIntent();
+        mActivityEditSmartDeviceBinding = getViewDataBinding();
+        mMainViewModel.setNavigator(this);
+        setUp();
+    }
 
-        deviceId = i.getIntExtra("device_id", 0);
+    private void setUp(){
+        //setContentView(R.layout.activity_edit_smart_device);
+        //Intent i = getIntent();
+
+        //deviceId = i.getIntExtra("device_id", 0);
 
         //UserPreference.getSmartDeviceById(getApplicationContext(), deviceId).observe(this, new Observer<SmartDevice>() {
         //    @Override
@@ -32,54 +52,81 @@ public class EditSmartDeviceActivity extends AppCompatActivity {
         //        if (smartDevice == null) {
         //            finish();
         //        }
-
-
-                final TextView textView = findViewById(R.id.textView_editdeviceName);
+        final TextView textView = findViewById(R.id.textView_editdeviceName);
         //textView.setText(smartDevice.getDeviceName());
-
-                final EditText editTextName = findViewById(R.id.editText_editname);
+        final EditText editTextName = findViewById(R.id.editText_editname);
         //editTextName.setText(smartDevice.getDeviceName());
-
-                final Switch enabledDisabled = findViewById(R.id.switch_deviceswitch);
+        final Switch enabledDisabled = findViewById(R.id.switch_deviceswitch);
         //enabledDisabled.setChecked(smartDevice.getActive());
+        editTextName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    //Remove focus and hide keyboard
 
-                editTextName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                        boolean handled = false;
-                        if (actionId == EditorInfo.IME_ACTION_DONE) {
-                            //Remove focus and hide keyboard
+                    textView.setText(editTextName.getText());
+                    findViewById(R.id.editSmartDeviceLayout).requestFocus();
 
-                            textView.setText(editTextName.getText());
-                            findViewById(R.id.editSmartDeviceLayout).requestFocus();
+                    //Hide keyboard
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
-                            //Hide keyboard
-                            InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 
-                            handled = true;
-                        }
-                        return handled;
-                    }
-                });
+        Button confirmButton = findViewById(R.id.button_editSmartdeviceConfirm);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString();
+                Boolean active = enabledDisabled.isChecked();
 
-                Button confirmButton = findViewById(R.id.button_editSmartdeviceConfirm);
-                confirmButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String name = editTextName.getText().toString();
-                        Boolean active = enabledDisabled.isChecked();
+                //smartDevice.setDeviceName(name);
+                //smartDevice.setActive(active);
 
-                        //smartDevice.setDeviceName(name);
-                        //smartDevice.setActive(active);
-
-                        //UserPreference.updateSmartDevice(getApplicationContext(), smartDevice);
-
-                        finish();
-                    }
-                });
-
-        //    }
-        //});
+                //UserPreference.updateSmartDevice(getApplicationContext(), smartDevice);
+                finish();
+            }
+        });
     }
+
+    @Override
+    public void handleError(Throwable throwable)
+    {
+        // handle error
+    }
+    public static Intent newIntent(android.content.Context context) {
+        Intent intent = new Intent(context, EditSmartDeviceActivity.class);
+        return intent;
+    }
+
+
+    public int getBindingVariable() {
+        return BR.viewModel;
+    }
+
+
+    public int getLayoutId() {
+        return R.layout.activity_edit_smart_device;
+    }
+
+    @Override
+    public AddSmartDeviceViewModel getViewModel() {
+        return null;
+    }
+
+    public boolean onNavigationItemSelected(@android.support.annotation.NonNull MenuItem item) {
+        return true;
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return fragmentDispatchingAndroidInjector;
+    }
+
 }
+
